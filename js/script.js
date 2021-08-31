02fd487529c485b76898bd490390e73d3c168c8e
Irfan Rizkianto Pratama <ipratama209@gmail.com>
2021-08-31 16:15:30 +0700

    adding jquery & Js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // event pada saat link di klik
 $('.page-scroll').on('click', function(e) {
 
+    //Jumbrotron
     // ambil isi href
     var tujuan = $(this).attr('href');
+    
     // tangkap elemen ybs
     var elemenTujuan =  $(tujuan);
 
@@ -13,5 +15,46 @@ $('.page-scroll').on('click', function(e) {
     },1000, 'easeInOutExpo');
 
     e.preventDefault();
+
+});
+
+
+
+//about
+$(window).on('load', function(){
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
+// paralax
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    //jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px,'+ wScroll/4 +'%)'
+
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px,'+ wScroll/2 +'%)'
+
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px,'+ wScroll/1.2 +'%)'
+
+    });
+
+    //portofolio
+    if (wScroll > $('.portofolio').offset().top - 300){
+        $('.portofolio .thumbnail').each(function(i) {
+            setTimeout(function() {
+                $('.portofolio .thumbnail').eq(i).addClass('muncul');
+            }, 300 * (i+1));
+        });
+
+        
+    }   
 
 });
